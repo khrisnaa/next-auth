a90c62033fef6fabdb2813b4ae7e9765321a87a7
khrisnaa <igdkhrisnaaa@gmail.com>
2024-05-13 14:04:12 +0800

    add email verification

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,14 +1,14 @@
 import { v4 as uuidv4 } from 'uuid';
 
-import { db } from './db';
+import { db } from '@/lib/db';
 import { getVerificationTokenByEmail } from '@/data/verification-tokens';
+import { getPasswordResetTokenByEmail } from '@/data/password-reset-token';
 
 export const generateVerificationToken = async (email: string) => {
   const token = uuidv4();
   const expires = new Date(new Date().getTime() + 1000 * 3600);
 
   const existingToken = await getVerificationTokenByEmail(email);
-
   if (existingToken) {
     await db.verificationToken.delete({ where: { id: existingToken.id } });
   }
@@ -22,4 +22,24 @@ export const generateVerificationToken = async (email: string) => {
   });
 
   return verificationToken;
+};
+
+export const generatePasswordResetToken = async (email: string) => {
+  const token = uuidv4();
+  const expires = new Date(new Date().getTime() + 1000 * 3600);
+
+  const existingToken = await getPasswordResetTokenByEmail(email);
+  if (existingToken) {
+    await db.passwordResetToken.delete({ where: { id: existingToken.id } });
+  }
+
+  const passwordResetToken = await db.passwordResetToken.create({
+    data: {
+      token,
+      email,
+      expires,
+    },
+  });
+
+  return passwordResetToken;
 };
--- routes.ts
+++ routes.ts
@@ -2,7 +2,13 @@
 
 export const publicRoutes = ['/', '/auth/new-verification'];
 
-export const authRoutes = ['/auth/login', '/auth/register', '/auth/error'];
+export const authRoutes = [
+  '/auth/login',
+  '/auth/register',
+  '/auth/error',
+  '/auth/reset',
+  '/auth/new-password',
+];
 
 export const apiRoutesPrefix = '/api/auth';
 
